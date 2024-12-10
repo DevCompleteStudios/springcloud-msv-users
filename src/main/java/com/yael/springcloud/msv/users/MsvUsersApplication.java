ae0e208b89ee579c2fdb84e27d8e9723ebe29231
DevCompleteStudios <jorge.yael333@gmail.com>
2024-12-10 10:08:28 -0600

    First commit

---
+++ src/main/java/com/yael/springcloud/msv/users/MsvUsersApplication.java
@@ -0,0 +1,13 @@
+package com.yael.springcloud.msv.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvUsersApplication.class, args);
+	}
+
+}
